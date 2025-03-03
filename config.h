c06e699608d9380a8e277a467577f3fd0c66c32d
Lisen Hatson <lisenhatson@gmail.com>
2025-03-03 10:48:48 +0700

    opaque

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const char *fonts[] = {
 	"monospace:size=10",
 	"NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
 };
-static const unsigned int bgalpha = 0xe0;
+static const unsigned int bgalpha = OPAQUE;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
